66653381505c3720ca9d38e5bf8fd72552fda4e3
klaatu23 <tengu23hotmail.com>
2021-07-12 12:39:11 -0500

    binding example

---
+++ app/src/main/java/com/example/binding/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.binding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.binding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnNoche.setOnClickListener {
+            Toast.makeText(this, "Buenas noches...", Toast.LENGTH_SHORT).show()
+            binding.texto.setText("Good morning")
+        }
+
+        binding.btnDia.setOnClickListener {
+            Toast.makeText(this, "Buen dia...", Toast.LENGTH_SHORT).show()
+            binding.texto.setText("Good night")
+        }
+
+    }
+}
